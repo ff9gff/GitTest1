b1d2a2121a097cb1a30c36d0ed3bdaaf01a1d084
stu <stu@itwill>
2016-11-11 16:08:14 +0900

    수정테스트입니다

---
+++ Project03/src/main/java/edu/spring/project03/TestClass92.java
@@ -0,0 +1,10 @@
+package edu.spring.project03;
+
+public class TestClass92 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("고유나 수정 test");
+	}
+
+}
