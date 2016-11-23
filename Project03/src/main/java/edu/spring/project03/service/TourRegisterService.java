509ab18e0506649ae074d6691474967a6243c310
TaeHoonKim1 <stu@itwill>
2016-11-23 18:17:52 +0900

    김태훈 tourRegister db 생성중

---
+++ Project03/src/main/java/edu/spring/project03/service/TourRegisterService.java
@@ -0,0 +1,9 @@
+package edu.spring.project03.service;
+
+import edu.spring.project03.domain.TourRegisterVO;
+
+public interface TourRegisterService {
+	
+	public abstract int create(TourRegisterVO vo);
+
+}
