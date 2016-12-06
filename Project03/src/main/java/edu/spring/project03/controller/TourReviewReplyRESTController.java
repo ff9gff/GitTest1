f51d39c6ec84e89693c8c8f8d5c051361b78fb85
stu <stu@DESKTOP-T50JSKS>
2016-12-06 10:21:58 +0900

    12/06 10:20 daeho default-profile image test

--- Project03/src/main/java/edu/spring/project03/controller/TourReviewReplyRESTController.java
+++ Project03/src/main/java/edu/spring/project03/controller/TourReviewReplyRESTController.java
@@ -1,14 +1,11 @@
-/*
+
 package edu.spring.project03.controller;
 
 import java.util.List;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +13,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.spring.project03.domain.PersonalVO;
-import edu.spring.project03.domain.TourReplyVO;
-
-import edu.spring.project03.service.TourReplyService;
+import edu.spring.project03.domain.ReviewReplyVO;
+import edu.spring.project03.service.TourReviewReplyService;
 
 @RestController
 @RequestMapping(value = "/review/detail/reply")
 public class TourReviewReplyRESTController {
-	// private static Logger logger =
-	// LoggerFactory.getLogger(TourReplyRESTController.class);
-
+/*
 	@Autowired
-	private TourReplyService service;
+	private TourReviewReplyService reviewreplyservice;
 
 	// 데이터 입력
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Integer> createReply(@RequestBody TourReplyVO vo) {
+	public ResponseEntity<Integer> createReply(@RequestBody ReviewReplyVO reviewreplyvo) {
 		ResponseEntity<Integer> entity = null;
-		int result = service.create(vo);
+		int result = reviewreplyservice.create(reviewreplyvo);
 		if (result == 1) { // DB insert 성공
 			entity = new ResponseEntity<Integer>(1, HttpStatus.OK);
 		} else { // DB insert 실패
@@ -45,13 +39,13 @@ public class TourReviewReplyRESTController {
 
 	// 해당 게시글의 모든 댓글 목록을 읽어오는 메소드
 	@RequestMapping(value = "/all/{no}", method = RequestMethod.GET)
-	public ResponseEntity<List<TourReplyVO>> readReplies(@PathVariable("no") Integer trip_no) {
-		List<TourReplyVO> list = service.read(trip_no);
-		ResponseEntity<List<TourReplyVO>> entity = null;
+	public ResponseEntity<List<ReviewReplyVO>> readReplies(@PathVariable("no") Integer review_no) {
+		List<ReviewReplyVO> list = reviewreplyservice.read(review_no);
+		ResponseEntity<List<ReviewReplyVO>> entity = null;
 		if (list != null) { // select 성공
-			entity = new ResponseEntity<List<TourReplyVO>>(list, HttpStatus.OK);
+			entity = new ResponseEntity<List<ReviewReplyVO>>(list, HttpStatus.OK);
 		} else { // select 실패
-			entity = new ResponseEntity<List<TourReplyVO>>(list, HttpStatus.BAD_REQUEST);
+			entity = new ResponseEntity<List<ReviewReplyVO>>(list, HttpStatus.BAD_REQUEST);
 		} // end if
 
 		return entity;
@@ -59,8 +53,8 @@ public class TourReviewReplyRESTController {
 	
 	// 해당 신청자의 프로필을 읽어오는 메소드
 	@RequestMapping(value="/person/{no}", method=RequestMethod.GET)
-	public ResponseEntity<List<PersonalVO>> readPerson(@PathVariable("no") Integer trip_no){
-		List<PersonalVO> list = service.readPerson(trip_no);
+	public ResponseEntity<List<PersonalVO>> readPerson(@PathVariable("no") Integer review_no){
+		List<PersonalVO> list = reviewreplyservice.readPerson(review_no);
 		ResponseEntity<List<PersonalVO>> entity = null;
 		if(list != null){ // select 성공
 			entity = new ResponseEntity<>(list, HttpStatus.OK);
@@ -72,11 +66,11 @@ public class TourReviewReplyRESTController {
 
 	// 해당 댓글을 수정
 	@RequestMapping(value = "/{no}", method = RequestMethod.PUT)
-	public ResponseEntity<String> updateReply(@PathVariable("no") Integer rno, @RequestBody TourReplyVO vo) {
+	public ResponseEntity<String> updateReply(@PathVariable("no") Integer rno, @RequestBody ReviewReplyVO reviewreplyvo) {
 		ResponseEntity<String> entity = null;
 
-		vo.setRno(rno);
-		int result = service.update(vo);
+		reviewreplyvo.setRno(rno);
+		int result = reviewreplyservice.update(reviewreplyvo);
 		if (result == 1) { // update 성공
 			entity = new ResponseEntity<String>("success", HttpStatus.OK);
 		} else { // update 실패
@@ -94,9 +88,9 @@ public class TourReviewReplyRESTController {
 		ResponseEntity<String> entity = null;
 		int result = 0;
 
-		result = service.delete(rno);
+		result = reviewreplyservice.delete(rno);
 		if (parentrno == 0) {
-			service.allDelete(rno);
+			reviewreplyservice.allDelete(rno);
 		}
 
 		if (result == 1) { // delete 성공
@@ -106,9 +100,7 @@ public class TourReviewReplyRESTController {
 		} // end if()
 
 		return entity;
-	}// end deleteReply()
+	}// end deleteReply()	
 	
-	
-
-}// end class TourReplyRESTController
 */
+}// end class TourReviewReplyRESTController
