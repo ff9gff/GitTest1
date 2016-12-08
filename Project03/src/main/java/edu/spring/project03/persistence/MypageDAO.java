95ea40720017b58c91430447f365fe5a3244b6c0
taehoon <taehoon@DESKTOP-IRKDS2B>
2016-12-08 16:08:27 +0900

    마이 페이지 후기게시글 리스트 수정정

--- Project03/src/main/java/edu/spring/project03/persistence/MypageDAO.java
+++ Project03/src/main/java/edu/spring/project03/persistence/MypageDAO.java
@@ -5,6 +5,8 @@ import java.util.List;
 import edu.spring.project03.domain.ImgVO;
 import edu.spring.project03.domain.PersonalVO;
 import edu.spring.project03.domain.RegionVO;
+import edu.spring.project03.domain.ReviewRegionVO;
+import edu.spring.project03.domain.ReviewVO;
 import edu.spring.project03.domain.TourRegisterVO;
 
 public interface MypageDAO {
@@ -18,6 +20,11 @@ public interface MypageDAO {
 	public abstract ImgVO selectProfile(int mno);
 	
 	public abstract List<ImgVO> select_join_mno(int mno);
+	
+	// 후기 게시글 리스트
+	public abstract List<ImgVO> select_review_mno(int mno);
+	public abstract List<ReviewVO> select_review_title(int mno);
+	public abstract List<ReviewRegionVO> select_review_region(int mno);
 	
 	public abstract int updateProfile(PersonalVO vo);
 	
